5c0f32dac8b5d56be9219dbcf4317c92f4ec71b5
Alvaro Aller <alvaro.aller@suse.com>
2026-09-16 11:10:49 +0200

    Update main.tf
    Add network_rule to restrict access to the Storage account

--- provider/azure/main.tf
+++ provider/azure/main.tf
@@ -11,13 +11,12 @@ provider "azurerm" {
   features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "ad_rg" {
   name     = "${var.environment}-ad-rg"
   location = var.azure_region
 }
 
-# Virtual Network
+
 resource "azurerm_virtual_network" "ad_vnet" {
   name                = "${var.environment}-ad-vnet"
   address_space       = [var.vnet_cidr]
@@ -25,7 +24,7 @@ resource "azurerm_virtual_network" "ad_vnet" {
   resource_group_name = azurerm_resource_group.ad_rg.name
 }
 
-# Subnet
+
 resource "azurerm_subnet" "ad_subnet" {
   name                 = "${var.environment}-ad-subnet"
   resource_group_name  = azurerm_resource_group.ad_rg.name
@@ -33,13 +32,13 @@ resource "azurerm_subnet" "ad_subnet" {
   address_prefixes     = [var.subnet_cidr]
 }
 
-# Network Security Group
+
 resource "azurerm_network_security_group" "ad_nsg" {
   name                = "${var.environment}-ad-nsg"
   location            = azurerm_resource_group.ad_rg.location
   resource_group_name = azurerm_resource_group.ad_rg.name
 
-  # RDP
+ 
   security_rule {
     name                       = "AllowRDP"
     priority                   = 100
@@ -52,7 +51,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # DNS TCP
+  
   security_rule {
     name                       = "AllowDNS-TCP"
     priority                   = 110
@@ -65,7 +64,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # DNS UDP
+ 
   security_rule {
     name                       = "AllowDNS-UDP"
     priority                   = 111
@@ -78,7 +77,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # LDAP TCP
+ 
   security_rule {
     name                       = "AllowLDAP-TCP"
     priority                   = 120
@@ -91,7 +90,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # LDAP UDP
+
   security_rule {
     name                       = "AllowLDAP-UDP"
     priority                   = 121
@@ -104,7 +103,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # LDAPS
+ 
   security_rule {
     name                       = "AllowLDAPS"
     priority                   = 130
@@ -117,7 +116,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # Kerberos TCP
+  
   security_rule {
     name                       = "AllowKerberos-TCP"
     priority                   = 140
@@ -130,7 +129,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # Kerberos UDP
+
   security_rule {
     name                       = "AllowKerberos-UDP"
     priority                   = 141
@@ -143,7 +142,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # SMB
+ 
   security_rule {
     name                       = "AllowSMB"
     priority                   = 150
@@ -156,7 +155,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # Global Catalog
+ 
   security_rule {
     name                       = "AllowGlobalCatalog"
     priority                   = 160
@@ -169,7 +168,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
     destination_address_prefix = "*"
   }
 
-  # WinRM
+  
   security_rule {
     name                       = "AllowWinRM"
     priority                   = 170
@@ -183,7 +182,7 @@ resource "azurerm_network_security_group" "ad_nsg" {
   }
 }
 
-# Public IP Address
+
 resource "azurerm_public_ip" "dc_pip" {
   name                = "${var.environment}-dc-pip"
   location            = azurerm_resource_group.ad_rg.location
@@ -192,7 +191,7 @@ resource "azurerm_public_ip" "dc_pip" {
   sku                 = "Standard"
 }
 
-# Network Interface
+
 resource "azurerm_network_interface" "dc_nic" {
   name                = "${var.environment}-dc-nic"
   location            = azurerm_resource_group.ad_rg.location
@@ -206,13 +205,13 @@ resource "azurerm_network_interface" "dc_nic" {
   }
 }
 
-# Associate NSG with NIC
+
 resource "azurerm_network_interface_security_group_association" "dc_nic_nsg" {
   network_interface_id      = azurerm_network_interface.dc_nic.id
   network_security_group_id = azurerm_network_security_group.ad_nsg.id
 }
 
-# Domain Controller Virtual Machine
+
 resource "azurerm_windows_virtual_machine" "domain_controller" {
   name                = "${var.environment}-dc"
   resource_group_name = azurerm_resource_group.ad_rg.name
@@ -239,13 +238,19 @@ resource "azurerm_windows_virtual_machine" "domain_controller" {
   }
 }
 
-# Storage Account for scripts (optional but more reliable)
+
 resource "azurerm_storage_account" "scripts" {
   name                     = "${var.environment}adscripts${random_string.storage_suffix.result}"
   resource_group_name      = azurerm_resource_group.ad_rg.name
   location                 = azurerm_resource_group.ad_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  network_rules {
+    default_action = "Deny"
+    ip_rules       = var.allowed_storage_ips
+    bypass         = ["AzureServices"]
+  }
 }
 
 resource "random_string" "storage_suffix" {
